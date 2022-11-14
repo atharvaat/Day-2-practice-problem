54a92901422fa0a9bc725ffe2047c0dfca7683d4
atharva <atharvatodankar.at@gmail.com>
2022-11-14 13:02:45 +0530

    To Display sum of natural Num

---
+++ SumOfNatualNum.java
@@ -0,0 +1,13 @@
+package Day2practiceproblem;
+
+public class SumOfNatualNum { 
+	public static void main(String[] args)   
+	{  
+	int i, num = 5, sum = 0;    
+	for(i = 1; i <= num; ++i)  
+	{   
+	sum = sum + i;  
+	}  
+	System.out.println("Sum of First 10 Natural Numbers is = " + sum);  
+	}  
+}
